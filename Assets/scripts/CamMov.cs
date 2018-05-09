4fccefe3c82442423b7f5a293f0ad121d039158c
Brahm <abraham.schultz@gmail.com>
2018-05-09 15:59:30 -0400

    Initial commit

---
+++ Assets/scripts/CamMov.cs
@@ -0,0 +1,44 @@
+﻿/*CammMov.cs
+ * Abraham Schultz
+ * 
+ * Last edited 5/9//2018
+ * 
+ * This is a script to move the camera in an endless run down a hallway
+ * 
+ * 
+ * 
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMov : MonoBehaviour {
+
+    
+    public float camPos;          // current point of camera z axis
+    public GameObject mainCamera;  // main cam
+    public Vector3 startCam;       // starting point of cam
+
+    // Use this for initialization
+    void Start () {
+     
+         mainCamera = GameObject.Find("Main Camera");
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        camPos = GetComponent<Rigidbody>().position.z;
+        if (camPos > 16.0f)                     // if cam gets to certain point, then send back to begining.
+        {
+
+           
+        mainCamera.transform.Translate(0, -3.037822f, -16);    // hardcoded starting location for debugging
+            camPos = 0;
+
+        }
+		
+	}
+}
---
+++ Assets/scripts/MovOrb.cs
@@ -0,0 +1,37 @@
+﻿/*MovOrb.cs
+ * Abraham Schultz
+ * 
+ * Last edited 5/9//2018
+ * 
+ * This is a script to move the player in an endless run down a hallway
+ * 
+ * 
+ * 
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovOrb : MonoBehaviour {
+
+    public GameObject player;     // game object for player
+    public GameObject mainCamera;
+    public float playerPos;      // Starting position of player
+
+    // Use this for initialization
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera");
+        player = GameObject.Find("player");
+    }// end start
+	
+	// Update is called once per frame
+	void Update () {
+        playerPos = GetComponent<Rigidbody>().position.z;          //set player position
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 4);  // move player hardcoded for debugging 
+
+
+    }// end update
+} // end MovOrb
