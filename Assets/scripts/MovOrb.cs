24d7f5dea95971f439be688f218b2d4be937d2b4
Brahm <abraham.schultz@gmail.com>
2018-05-10 14:02:33 -0400

    added movement.
    added key coded for left and right movement.

--- Assets/scripts/MovOrb.cs
+++ Assets/scripts/MovOrb.cs
@@ -16,9 +16,14 @@ using UnityEngine;
 
 public class MovOrb : MonoBehaviour {
 
+    //movements and controls
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+    //objects in game
     public GameObject player;     // game object for player
     public GameObject mainCamera;
     public float playerPos;      // Starting position of player
+    public float horizVel = 0;   //hortizontal velocity of player
 
     // Use this for initialization
     void Start()
@@ -28,10 +33,34 @@ public class MovOrb : MonoBehaviour {
     }// end start
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         playerPos = GetComponent<Rigidbody>().position.z;          //set player position
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 4);  // move player hardcoded for debugging 
+        GetComponent<Rigidbody>().velocity = new Vector3(horizVel, 0, 4);  // move player hardcoded for debugging 
 
+
+        //if player presses left button
+        if (Input.GetKeyDown(moveLeft)) {
+
+            horizVel = -2;                //move left
+            StartCoroutine(StopSlide());  // stop move
+        }// end if
+
+        //if player presses right button
+        if (Input.GetKeyDown(moveRight))
+        {
+
+            horizVel = +2;                //move left
+            StartCoroutine(StopSlide());  // stop move
+        }// end if
 
     }// end update
+
+    // method to stop player after half second of horizontal movement. 
+    IEnumerator StopSlide() {
+
+        yield return new WaitForSeconds(.5f);
+        horizVel = 0;
+    }// end stopSlide
+
 } // end MovOrb
