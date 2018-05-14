33756366ad23490b8f9a73045d10003a5395523d
Brahm <abraham.schultz@gmail.com>
2018-05-14 16:22:21 -0400

    added myo armband support
    added myo armband support, left and right movement for now.

--- Assets/scripts/MovOrb.cs
+++ Assets/scripts/MovOrb.cs
@@ -13,6 +13,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
 
 public class MovOrb : MonoBehaviour {
 
@@ -27,6 +31,15 @@ public class MovOrb : MonoBehaviour {
     public int laneNumber = 2;           // hold current lane player is in. L= 1 , M=2 , R =3
     private bool controlLocked = false; // to prevent rapid button mashing of controls. when false controls work.
 
+
+    // Myo game object to connect with.
+    // This object must have a ThalmicMyo script attached.
+    public GameObject myo = null;
+    // The pose from the last update. This is used to determine if the pose has changed
+    // so that actions are only performed upon making them rather than every frame during
+    // which they are active.
+    private Pose _lastPose = Pose.Unknown;
+
     // Use this for initialization
     void Start()
     {
@@ -39,6 +52,42 @@ public class MovOrb : MonoBehaviour {
     {
         playerPos = GetComponent<Rigidbody>().position.z;                   //set player position
         GetComponent<Rigidbody>().velocity = new Vector3(horizVel, 0, 4);  // move player hardcoded for debugging 
+
+        // Access the ThalmicMyo component attached to the Myo object.
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+
+        // Update references when the pose becomes fingers spread or the q key is pressed.
+        
+
+
+        // input to move ball from myo armband
+        if (thalmicMyo.pose != _lastPose)
+        {
+            _lastPose = thalmicMyo.pose;
+
+            if (thalmicMyo.pose == Pose.WaveIn)
+            {
+                
+
+                horizVel = -2;                //move left
+                StartCoroutine(StopSlide());  // stop move
+                laneNumber -= 1;
+                controlLocked = true;
+                ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            } // end if
+
+            if (thalmicMyo.pose == Pose.WaveOut)
+            {
+                
+
+                horizVel = +2;                //move left
+                StartCoroutine(StopSlide());  // stop move
+                laneNumber += 1;
+                controlLocked = true;
+                ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            } // end if
+        }// end if
+
 
 
         //if player presses left button and is not in left most lane already.
@@ -70,4 +119,20 @@ public class MovOrb : MonoBehaviour {
         controlLocked = false;
     }// end stopSlide
 
+
+
+    // Extend the unlock if ThalmcHub's locking policy is standard, and notifies the given myo that a user action was
+    // recognized.
+
+    void ExtendUnlockAndNotifyUserAction(ThalmicMyo myo)
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (hub.lockingPolicy == LockingPolicy.Standard)
+        {
+            myo.Unlock(UnlockType.Timed);
+        }
+
+        myo.NotifyUserAction();
+    }
 } // end MovOrb
