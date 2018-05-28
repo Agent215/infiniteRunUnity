dbafcedac6033f04d9708067867120ba003ee8d0
Brahm <abraham.schultz@gmail.com>
2018-05-28 16:11:04 -0400

    PreFab instantiation
    got rid of old prefabs. created new gamecontrol script to instantiate objects as needed. the spawn point does not work correctly now. but will be fixed soon

--- Assets/scripts/CamMov.cs
+++ Assets/scripts/CamMov.cs
@@ -17,24 +17,46 @@ using UnityEngine;
 public class CamMov : MonoBehaviour {
 
     
-    public float camPos;          // current point of camera z axis
+    public float camPos;          // current point of camera 
     public GameObject mainCamera;  // main cam
     private Vector3 spawnLocation;  // location cam spawns at after resetting 
+    private GameObject ground;      // hold ground object
+    private Vector3 groundStart;    //exact location where the ground object begins
+    private float groundZ;  // starting point in the z axis for ground
+    private float groundY;  // should not change
+    private float groundX;  // should not change
+
 
     //******************************************************************************************************************************
     // Use this for initialization
     void Start () {
-
-    
+                                     
         mainCamera = GameObject.Find("Main Camera");                // get the camera object
         spawnLocation = mainCamera.transform.position;              // get starting position of cam
         GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 4);  // move the camera foward 
-	} // end start
+        ground = GameObject.Find("ground(Clone)");
+    } // end start
 
     //******************************************************************************************************************************
     // Update is called once per frame
     void Update() {
-        camPos = GetComponent<Rigidbody>().position.z;
+
+        
+
+        groundX = ground.transform.position.x;
+        groundY = ground.transform.position.y;
+        camPos = mainCamera.transform.position.z;
+        groundZ = ground.transform.position.z - 2* ground.transform.position.z;  // starting point in z axis
+        groundStart = new Vector3(groundX, groundY, groundZ);        
+
+
+        if (camPos > ground.transform.position.z ) {
+
+
+            mainCamera.transform.position = (groundStart);  // move to begining of ground
+        }
+
+
 
 
     } //end update
---
+++ Assets/scripts/GameControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public Transform ground;   //locations of ground prefab
+    GameObject player;  // object to hold player
+    Vector3 playerPos;  // player position
+
+	// Use this for initialization
+	void Start () {
+        
+        player = GameObject.Find("player");
+        playerPos = player.transform.position;
+        Instantiate(ground,playerPos, ground.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerPos = player.transform.position;
+	}
+}
