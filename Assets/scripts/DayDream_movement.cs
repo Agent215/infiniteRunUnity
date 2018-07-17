3178b6295e87f62a848e3554c2dede2657dafcf7
Brahm <abraham.schultz@gmail.com>
2018-07-17 15:41:47 -0400

    google DayDream Controller support

---
+++ Assets/scripts/DayDream_movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayDream_movement : MonoBehaviour {
+
+    private Vector2 CurrentTouch;
+    public GameObject Controller;
+
+	// Use this for initialization
+	void Start () {
+
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CurrentTouch = GvrControllerInput.TouchPos;
+
+
+	}
+}
--- Assets/scripts/UI/ComputerDisplay.cs
+++ Assets/scripts/UI/ComputerDisplay.cs
@@ -6,58 +6,132 @@
  * 
  * */
 
+namespace GoogleVR.HelloVR
+{
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using UnityEngine.SceneManagement;
 
-public class ComputerDisplay : MonoBehaviour {
+    [RequireComponent(typeof(Collider))]
 
-    public string displayString;
-    public Text myText;
-    public float fadeTime;
-    public bool displayInfo;
-   
-
-
-	// Use this for initialization
-	void Start () {
-        myText = GameObject.Find("Computer Text").GetComponent<Text>();
-        myText.color = Color.clear;
-	} // end start 
-	
-	// Update is called once per frame
-	void Update () {
-        FadeText();
-
-    } // end update 
-
-    //display text when cursor is over object
-    private void OnMouseOver()
+    public class ComputerDisplay : MonoBehaviour
     {
-        displayInfo = true;
-    } // end OnMouseOver
 
-    // hide text when cursor is not on object
-    private void OnMouseExit()
-    {
-        displayInfo = false;
-    } // end onMouseExit
+        public string displayString;
+        public Text myText;
+        public float fadeTime;
+        public bool displayInfo;
 
-    // method to fade text in and out of UI
-    void FadeText() {
+        public Material LookingAtComp;     //material to display when looking at computer
+        public Material NotLookingAtComp;  // material to display when not looking at computer
 
-        if (displayInfo)
+        private Renderer myRenderer;
+
+
+        // Use this for initialization
+        void Start()
         {
-            myText.text = displayString;
-            myText.color = Color.Lerp(myText.color, Color.black, fadeTime * Time.deltaTime);
+            myText = GameObject.Find("Computer Text").GetComponent<Text>();
+            myText.color = Color.clear;
+            GetComponent<Renderer>().material = NotLookingAtComp;
+            myRenderer = GetComponent<Renderer>();
+            SetGazedAt(false);
+        } // end start 
 
-        } // end if
-        else {
 
-            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        // Update is called once per frame
+        void Update()
+        {
+            FadeText();
+
+        } // end update 
+
+        //#if (UNITY_EDITOR || UNITY_STANDALONE_WIN)
+        //    // when running on editor or windows
+
+        //    //display text when cursor is over object
+        //    private void OnMouseOver()
+        //    {
+        //        displayInfo = true;
+        //        GetComponent<Renderer>().material = LookingAtComp;
+        //    } // end OnMouseOver
+
+        //    // hide text when cursor is not on object
+        //    private void OnMouseExit()
+        //    {
+        //        displayInfo = false;
+        //        GetComponent<Renderer>().material = NotLookingAtComp;
+        //    } // end onMouseExit
+
+        //#endif
+
+
+        public void SetGazedAt(bool gazedAt)
+        {
+            if (NotLookingAtComp != null && LookingAtComp != null)
+            {
+                myRenderer.material = gazedAt ? LookingAtComp : NotLookingAtComp;
+                return;
+            }
         }
-    } // end FadeText
 
-}// end ComputerDisplay
+#if UNITY_ANDROID
+        //for Vr on Android
+
+        public void OnPointerOver() { displayInfo = true; GetComponent<Renderer>().material = LookingAtComp; Debug.Log("pointer over computer screen"); } // end OnPointerOver
+
+        public void OnPointerExit() { displayInfo = false; GetComponent<Renderer>().material = NotLookingAtComp; Debug.Log("Pointer Not over computer screen"); } // end OnPointerExit
+
+        public void OnPointerClick() { SceneManager.LoadScene("hallway 1"); } // end OnPointerClick
+
+
+
+        
+#endif
+
+
+
+        // method to fade text in and out of UI
+        void FadeText()
+        {
+
+            if (displayInfo)
+            {
+                myText.text = displayString;
+                myText.color = Color.Lerp(myText.color, Color.black, fadeTime * Time.deltaTime);
+
+            } // end if
+            else
+            {
+
+                myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+            }
+        } // end FadeText
+
+
+
+
+        public void Recenter()
+        {
+#if !UNITY_EDITOR
+      GvrCardboardHelpers.Recenter();
+#else
+            if (GvrEditorEmulator.Instance != null)
+            {
+                GvrEditorEmulator.Instance.Recenter();
+            }
+#endif  // !UNITY_EDITOR
+        }
+
+        public void ChangeScene()
+        {
+
+            SceneManager.LoadScene("hallway 1");
+
+        }
+
+    }// end ComputerDisplay
+}
